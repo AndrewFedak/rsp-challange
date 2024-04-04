59fb73da6a3b55853aa3271241a118c6625413a5
AndrewFedak <fedakandrew@gmail.com>
2024-04-04 20:08:26 +0300

    migration to postgres

---
+++ libs/PostgresDatabase/prisma/migrations/20240404164015_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Game" DROP CONSTRAINT "Game_winnerId_fkey";
+
+-- AlterTable
+ALTER TABLE "Game" ALTER COLUMN "winnerId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Player" ALTER COLUMN "choice" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_winnerId_fkey" FOREIGN KEY ("winnerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
